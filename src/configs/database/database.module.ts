48ac9cf7b9d5bc07f6687000c2e1073fff5c247d
Diego-Tapia <95628579+Diego-Tapia@users.noreply.github.com>
2021-12-09 12:09:40 -0300

    Procfile heroku, run pipeline

--- src/configs/database/database.module.ts
+++ src/configs/database/database.module.ts
@@ -1,24 +1,15 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import configs from '../environments/configs';
-
 @Global()
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigType<typeof configs>) => {
-        const { engine, name, host, port, user, pass } = configService.database;
-        return {
-          uri: `${engine}://${host}:${port}`,
-          user,
-          pass,
-          dbName: name,
-        };
-      },
-      inject: [configs.KEY],
-    }),
+      useFactory: () => ({
+        uri: process.env.URI,
+      }),
+    })
   ],
 })
 export class DatabaseModule {}
