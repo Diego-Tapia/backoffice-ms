46589599d673f1889bc485a5bba17dbd19faccf3
Diego-Tapia <95628579+Diego-Tapia@users.noreply.github.com>
2021-12-09 12:49:23 -0300

    update validations

--- src/configs/environments/env.validations.ts
+++ src/configs/environments/env.validations.ts
@@ -3,8 +3,6 @@ import * as Joi from 'joi';
 export default Joi.object({
   PORT: Joi.number().required(),
   SQS_HOST: Joi.string().required(),
-  SQS_USER: Joi.string().required(),
-  SQS_PASS: Joi.string().required(),
   SQS_T_URL: Joi.string().required(),
   AWS_ACCESS_KEY_ID: Joi.string().required(),
   AWS_SECRET_ACCESS_KEY: Joi.string().required(),
